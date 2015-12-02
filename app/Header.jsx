6e8b51a034f430beebf52eca9fed7066f8689dc1
Ariel Hou <arielqhou@gmail.com>
2015-12-02 16:42:39 -0500

    Add link to bootstrap stylesheet.

---
+++ app/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {render} from 'react-dom';
+import {Jumbotron} from 'react-bootstrap';
+
+module.exports = React.createClass({
+  render(){
+    return (
+      <Jumbotron>
+        <h1>mucking about with react bootstrap</h1>
+      </Jumbotron>
+    )
+  }
+});
--- app/Hello.jsx
+++ app/Hello.jsx
@@ -1,9 +1,20 @@
 'use strict'
 import React from 'react';
+import {ButtonToolbar, Button} from 'react-bootstrap';
+import Header from './header';
+
 
 module.exports = React.createClass({
     displayName: 'HelloReact',
     render: function(){
-        return <div>Holy cow this was so difficult</div>
+        return (
+          <div>
+            <Header />
+            <ButtonToolbar>
+              <Button bsStyle="primary" bsSize="large">Primary</Button>
+              <Button>default</Button>
+            </ButtonToolbar>
+          </div>
+        )
     }
 });
